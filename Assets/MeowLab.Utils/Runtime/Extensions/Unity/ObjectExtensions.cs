3250a603d0c1414297c6c3dd3ca7d7dfdcfa8351
Alexey Shnigir <xilas8k@gmail.com>
2023-03-20 18:39:51 +0300

    up

--- Assets/MeowLab.Utils/Runtime/Extensions/Unity/ObjectExtensions.cs
+++ Assets/MeowLab.Utils/Runtime/Extensions/Unity/ObjectExtensions.cs
@@ -6,7 +6,7 @@ namespace MeowLab.Utils
     public static class ObjectExtensions
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static T AsDontDestroyable<T>(T obj) where T : Object {
+        public static T AsDontDestroyable<T>(this T obj) where T : Object {
             Object.DontDestroyOnLoad(obj);
             return obj;
         }
